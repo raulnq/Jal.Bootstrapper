b1d23983d9bac33e245c540d0c64cafeb5f5f717
Raul Naupari <raul.naupari@cignium.com>
2016-06-06 12:04:53 -0500

    refactoring

--- pack.bat
+++ pack.bat
@@ -1,7 +1,5 @@
 packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Bootstrapper\Jal.Bootstrapper.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Bootstrapper.Nuget
 
-packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Bootstrapper.AssemblyFinder\Jal.Bootstrapper.AssemblyFinder.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Bootstrapper.Nuget
-
 packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Bootstrapper.AutoMapper\Jal.Bootstrapper.AutoMapper.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Bootstrapper.Nuget
 
 packages\NuGet.CommandLine.2.8.6\tools\nuget pack Jal.Bootstrapper.CastleWindsor\Jal.Bootstrapper.CastleWindsor.csproj -Properties "Configuration=Release;Platform=AnyCPU;OutputPath=bin\Release" -Build -IncludeReferencedProjects -OutputDirectory Jal.Bootstrapper.Nuget
